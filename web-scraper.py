e5be31165cf01ceea185ca3bed206c19db4363ed
xreth <168580309+xreth@users.noreply.github.com>
2024-05-25 07:56:41 +0100

    Update web-scraper.py

--- web-scraper.py
+++ web-scraper.py
@@ -29,7 +29,6 @@ try:
         email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
         emails = re.findall(email_pattern, response.text)
 
-        
         # Print the emails if found, otherwise print NA
         if emails:
             print("\nEmails:")
